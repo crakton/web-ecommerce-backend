ca24c9896f19d78c9fac1ab7af26a9986e9afa01
crakton <redemptionjonathan@outlook.com>
2025-05-03 12:29:44 +0100

    Add blog management routes and model for CRUD operations

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import reviewRoutes from "./routes/review.route";
 import sellerRoutes from "./routes/seller.route";
 import uploadRoutes from "./routes/image.route";
 import paymentRoutes from "./routes/payment.route";
+import blogRoutes from "./routes/blog.route";
 import fileUpload from "express-fileupload";
 import fs from "fs";
 import path from "path";
@@ -72,7 +73,7 @@ app.use("/reviews", reviewRoutes);
 app.use("/admin", sellerRoutes);
 app.use("/upload", uploadRoutes);
 app.use("/payments", paymentRoutes);
-
+app.use("/blogs", blogRoutes);
 interface ShiprocketOrderRequest {
 	cart_data: {
 		items: Array<{
